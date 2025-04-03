98985418fd1051fcb9590556db6ac90b214441a3
Dmytro Teren <leo20091986@gmail.com>
2025-04-03 23:01:58 +0300

    add styles gallery

--- src/js/render-function.js
+++ src/js/render-function.js
@@ -12,7 +12,13 @@ const gallery = document.querySelector(".gallery");
 const createGallery = (images)=> {
     const constructor = images.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => 
        `<li class="image-item">
-            <div class="gallery-card"><a href="${webformatURL}"><img src="${largeImageURL}" alt="${tags}"/></a></div>
+            <div class="gallery-card"><a href="${webformatURL}"><img src="${largeImageURL}" alt="${tags}" class="image"/></a></div>
+            <ul class="image-info">
+                <li>Likes<p>${likes}</p></li>
+                <li>Views<p>${views}</p></li>
+                <li>Comments<p>${comments}</p></li>
+                <li>Downloads<p>${downloads}</p></li>
+            </ul>
         </li>`).join("");
     const galleryConstructor = new SimpleLightbox('.gallery-card a', { captionDelay: "250" });
     gallery.innerHTML = constructor;
