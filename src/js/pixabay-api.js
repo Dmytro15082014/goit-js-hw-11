5b8829a5a23196df7673b6e83150623b9830b5f6
Dmytro Teren <leo20091986@gmail.com>
2025-04-03 13:31:08 +0300

    add search & loader styles

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,3 +1,7 @@
 // У файлі pixabay-api.js зберігай функції для виконання HTTP-запитів:
 
 // getImagesByQuery(query). Ця функція повинна приймати один параметр query (пошукове слово, яке є рядком), здійснювати HTTP-запит і повертати значення властивості data з отриманої відповіді.
+
+import axios from "axios";
+API_KEY = "49627345-81d87c81626646e8cd4c1e6ab";
+axios.defaults.baseURL = 'pixabay.com/api';
